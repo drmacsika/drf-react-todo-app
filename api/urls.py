c204c8fdb5ca03552719004669b95eedee24e9a2
Archangel Macsika <drmacsika@gmail.com>
2021-06-17 09:17:16 +0100

    Setting up the API with DRF

---
+++ api/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import include, path
+from todo.urls import router
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include(router.urls))
+]
